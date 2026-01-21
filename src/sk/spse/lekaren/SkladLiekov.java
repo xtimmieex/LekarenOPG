a087bbea8473c7a67ecc22619ceddb3de3f14932
daniel <slavokravec81@gmail.com>
2026-01-21 18:33:12 +0100

    Dokoncenie triedy SkladLiekov - naskladnenie a vyradenie po expiracii

--- src/sk/spse/lekaren/SkladLiekov.java
+++ src/sk/spse/lekaren/SkladLiekov.java
@@ -1,5 +1,6 @@
 package sk.spse.lekaren;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,26 +8,39 @@ public class SkladLiekov {
 
     private List<Liek> lieky;
 
+    // Konštruktor
     public SkladLiekov() {
         this.lieky = new ArrayList<>();
     }
 
+    // Getter
     public List<Liek> getLieky() {
         return lieky;
     }
 
+    // Setter
     public void setLieky(List<Liek> lieky) {
         this.lieky = lieky;
     }
 
+    // Naskladnenie lieku
     public void naskladniLiek(Liek liek) {
         lieky.add(liek);
     }
 
+    // Odstránenie konkrétneho lieku
     public void odstranLiek(Liek liek) {
         lieky.remove(liek);
     }
 
+    // Vyradenie liekov po expirácii
+    public void vyradLiekyPoExpiracii() {
+        lieky.removeIf(liek ->
+                liek.getDatumExpiracie().isBefore(LocalDate.now())
+        );
+    }
+
+    // Počet liekov v sklade
     public int getPocetLiekov() {
         return lieky.size();
     }
