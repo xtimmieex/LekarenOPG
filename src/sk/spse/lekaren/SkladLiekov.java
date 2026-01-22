540279bf81a17ce70153b88cc0e853e747ad2fb0
daniel <slavokravec81@gmail.com>
2026-01-22 14:44:59 +0100

    Pridanie pomocnej metody na zistenie prazdneho skladu

--- src/sk/spse/lekaren/SkladLiekov.java
+++ src/sk/spse/lekaren/SkladLiekov.java
@@ -8,40 +8,37 @@ public class SkladLiekov {
 
     private List<Liek> lieky;
 
-    // Konštruktor
     public SkladLiekov() {
         this.lieky = new ArrayList<>();
     }
 
-    // Getter
     public List<Liek> getLieky() {
         return lieky;
     }
 
-    // Setter
     public void setLieky(List<Liek> lieky) {
         this.lieky = lieky;
     }
 
-    // Naskladnenie lieku
     public void naskladniLiek(Liek liek) {
         lieky.add(liek);
     }
 
-    // Odstránenie konkrétneho lieku
     public void odstranLiek(Liek liek) {
         lieky.remove(liek);
     }
 
-    // Vyradenie liekov po expirácii
     public void vyradLiekyPoExpiracii() {
         lieky.removeIf(liek ->
                 liek.getDatumExpiracie().isBefore(LocalDate.now())
         );
     }
 
-    // Počet liekov v sklade
     public int getPocetLiekov() {
         return lieky.size();
+    }
+
+    public boolean jeSkladPrazdny() {
+        return lieky.isEmpty();
     }
 }
